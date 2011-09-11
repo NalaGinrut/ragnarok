908e794638ce4fa87ff393e4a9fd0e3f74aff41c
NalaGinrut <NalaGinrut@gmail.com>
2011-09-11 10:51:37 +0800

    ADD path_fix

--- cee/path_fix.c
+++ cee/path_fix.c
@@ -16,10 +16,130 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <libguile.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+#define MAX_PATH_LEN 4096 // exceeded would be cut
+
+#define NORMAL 1
+#define RELATIVE 2
+#define LAST 0
+#define END 0
+
+#define IS_END(str) (*(str) == '\0')
+
+#define IS_RELATIVE(path) \
+  ((path)[1] == '.' && (path)[2] == '.' \
+   && ((path)[3] == '/' || (path)[3] == '\0'))
+
+static inline int drop_last_dir(char* path)
+{
+  char* ptr = path-1;
+
+  if(ptr[0] == '\n')
+    return 0;
+    
+  while(*ptr != '/')
+    ptr--;
+
+  return path - ptr;
+}
+
+static inline int get_dir(const char* path ,char* buf ,int* pi ,int* bi)
+{
+  const char* head = NULL;
+  const char* tail = NULL;
+  int len = 0;
+  int path_len = 0;
+  int ret = 0;
+  
+  if(IS_END(path))
+    return END;
+
+  path += *pi;
+  buf += *bi;
+  
+  path_len = strlen(path);
+  head = path;
+  tail = (path_len-1) >= 0 ? memchr(path+1 ,'/' ,path_len-1) : NULL;
+
+  if(!tail)  // so it must be the last dir
+    {
+      len = path_len;
+
+      if(IS_RELATIVE(head))
+	{
+	  buf -= drop_last_dir(buf);
+	  buf[0] = '\0';
+	  return END;
+	}
+      
+      buf[len+1] = '\0'; // end fixed string
+      ret = LAST;
+    }
+  else if(IS_RELATIVE(head))
+    {
+      *bi -= drop_last_dir(buf);
+      *pi += 3; // length of "/.."
+      return RELATIVE;
+    }
+  else // not relative path
+    {
+      len = tail - head;
+      ret = NORMAL;
+    }
+
+  memcpy(buf ,head ,len);
+  *bi += len;
+  *pi += len;
+  
+  return ret;
+}
+
+SCM_DEFINE(scm_mmr_path_fix ,"path-fix" ,1,
+	   (SCM target),
+	   "deal with relative path for ragnarok server."
+	   )
+#define FUNC_NAME s_scm_mmr_path_fix
+{
+  char *path = NULL;
+  char *fixed = NULL; // fiexed path
+  int path_len = 0;
+  int bi = 0;
+  int pi = 0;
+  
+  SCM_VALIDATE_STRING(1, target);
+
+  path = scm_to_locale_string(target);
+  path_len = strlen(path);
+  path_len = path_len>MAX_PATH_LEN? MAX_PATH_LEN : path_len;
+  fixed = (char *)calloc(1 ,path_len);
+  fixed[0] = '\n'; // sentinal
+
+  if(!memchr(path ,'.' ,strlen(path)))
+    return SCM_BOOL_F; // no relative path
+
+  while(get_dir(path ,fixed+1 ,&pi ,&bi))
+    {}
+
+  /* NOTE: The result won't contain '/' at the end,
+   * because we'll append *path '/' filenam* finally.
+   */
+
+  ret = scm_from_locale_string(fixed);
+  free(fixed);
+  fixed = NULL;
+
+  return ret;
+}
+#undef FUNC_NAME
+      
 // TODO: regenerate a valid path from request's path
 //	 1. cut off all the "..";
 //	 2. keep path within the root path;
 //	 3. deal with relative path
+
 
 
