c784d71f4c62e922c29b0a0531c19d00f2159fc4
NalaGinrut <NalaGinrut@gmail.com>
2011-10-13 10:37:33 +0800

    * Makefile.in: Fix temporary lib path while make utils.scm.go. * ragnarok/cgi/cgi.scm: add cgi env vars.

--- ragnarok/cgi/cgi.scm
+++ ragnarok/cgi/cgi.scm
@@ -16,9 +16,12 @@
 (define-module (ragnarok cgi cgi)
   #:use-module (ragnarok protocol http status)
   #:use-module (ragnarok utils)
+  #:use-module (ragnarok version)
   #:use-module (srfi srfi-9)
   #:export (make-cgi-record
 	    cgi-record?
+	    cgi-env-set!
+	    create-cgi
 	    regular-cgi-run
 	    )
   )
@@ -31,14 +34,100 @@
     (vector-set! st file-stat:size size)
     st
     ))
- 
+
+;; WARN: DO NOT change the order of *cgi-env-vars-list*.
+(define *cgi-env-vars-list*
+  '("AUTH_TYPE"
+    "CONTENT_LENGTH"
+    "CONTENT_TYPE"
+    "GATEWAY_INTERFACE"
+    "PATH_INFO"
+    "PATH_TRANSLATED"
+    "QUERY_STRING"
+    "REMOTE_ADDR"
+    "REMOTE_HOST"
+    "REMOTE_IDENT"
+    "REMOTE_USER"
+    "REQUEST_METHOD"
+    "SCRIPT_NAME"
+    "SERVER_NAME"
+    "SERVER_PORT"
+    "SERVER_PROTOCOL"
+    "SERVER_SOFTWARE"))
+
+
 (define-record-type cgi-record
-  (make-cgi-record target q-str conn-socket)
+  (make-cgi-record target env-table conn-socket)
   cgi-record?
   (target cgi:target)
-  (q-str cgi:q-str)
+  (env-table cgi:env-table) ;; a hash table to put envion vars
   (conn-socket cgi:conn-socket)
   )
+
+(define create-cgi
+  (lambda* 
+   (target conn-socket 
+	   #:key 
+	   (QUERY_STRING "")
+	   (REQUEST_METHOD "GET")
+	   (AUTH_TYPE "Basic")
+	   (CONTENT_LENGTH "")
+	   (CONTENT_TYPE "")
+	   (GATEWAY_INTERFACE "CGI")
+	   (PATH_INFO "")
+	   (PATH_TRANSLATED "")
+	   (REMOTE_ADDR "")
+	   (REMOTE_HOST "")
+	   (REMOTE_IDENT "")
+	   (REMOTE_USER "")
+	   (SCRIPT_NAME "")
+	   (SERVER_NAME "")
+	   (SERVER_PORT "80")
+	   (SERVER_PROTOCOL "http")
+	   (SERVER_SOFTWARE *ragnarok-version*)
+	   )
+  ;; WARN: DO NOT change this order.
+  (let* ([in `(,AUTH_TYPE
+	       ,CONTENT_LENGTH
+	       ,CONTENT_TYPE
+	       ,GATEWAY_INTERFACE
+	       ,PATH_INFO
+	       ,PATH_TRANSLATED
+	       ,QUERY_STRING
+	       ,REMOTE_ADDR
+	       ,REMOTE_HOST
+	       ,REMOTE_IDENT
+	       ,REMOTE_USER
+	       ,REQUEST_METHOD
+	       ,SCRIPT_NAME
+	       ,SERVER_NAME
+	       ,SERVER_PORT
+	       ,SERVER_PROTOCOL
+	       ,SERVER_SOFTWARE)]
+	 [env-table (make-hash-table 17)]
+	 )
+     
+    (for-each (lambda (e i)
+		(hash-set! env-table e i))
+	      *cgi-env-vars-list*
+	      in)
+    
+    (make-cgi-record target
+		     env-table
+		     conn-socket)
+    )))
+
+(define cgi-env-get
+  (lambda (key cgi)
+    (let ([env-table (cgi:env-table cgi)])
+      (hash-ref env-table key)
+      )))
+
+(define cgi-env-set!
+  (lambda (cgi key value)
+    (let ([env-table (cgi:env-table cgi)])
+      (hash-set! env-table key value)
+      )))
 
 (define regular-cgi-run
   (lambda (cgi)
@@ -54,19 +143,16 @@
 	(values #f *Fork-Error* #f))
        ((= i 0)
 	(let* ([target (cgi:target cgi)]
-	       [q-str (cgi:q-str cgi)]
+	       [QUERY_STRING (cgi-env-get 'QUERY_STRING cgi)]
 	       [conn-socket (cgi:conn-socket cgi)]
-	       [hole (open "/dev/null" O_RDWR)]
 	       )
-	  ;;(setvbuf w _IONBF) ;; set to block buffer
-	  ;;(redirect-port hole (current-error-port))
-	  ;;(redirect-port hole (current-input-port))
+	  (setvbuf w _IONBF) ;; set to block buffer
 	  (redirect-port w (current-output-port))
-	  ;;(redirect-port (current-output-port) w)
-
-	  ;; if q-str is not #f ,that means method is POST
-	  (if q-str 
-	      (setenv "QUERY_STRING" q-str)
+	  
+	  ;; if QUERY_STRING is not #f ,that means method is POST
+	  ;; FIXME: we should use REQUEST_METHOD to decide.
+	  (if QUERY_STRING 
+	      (setenv "QUERY_STRING" QUERY_STRING)
 	      (redirect-port conn-socket (current-input-port)))
 	  
 	  (execle target (environ)) ;; run cgi script
