353f8fa605ca8520f7c6f472f5cbc625e1731b4b
NalaGinrut <NalaGinrut@gmail.com>
2011-10-26 13:18:32 +0800

    regular fix

---
+++ autogen.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Usage: sh -x ./autogen.sh
+
+set -e
+
+[ -f RAGNAROK-VERSION ] || {
+  echo "autogen.sh: run this command only at the top of guile-core."
+  exit 1
+}
+
+######################################################################
+### announce build tool versions
+echo ""
+autoconf --version
+echo ""
+automake --version
+echo ""
+if test "`uname -s`" = Darwin; then
+  glibtool --version
+else
+  libtool --version
+fi
+echo ""
+${M4:-m4} --version
+echo ""
+flex --version
+echo ""
+
+######################################################################
+### update infrastructure
+
+autoreconf -i --force --verbose
+
+echo "Now run configure and make."
