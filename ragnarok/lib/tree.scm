db51f93da0ac7a24bb5c6aa997ecd8e613a3e696
Nala Ginrut <nalaginrut@gmail.com>
2012-11-13 18:28:07 +0800

    modified:   ragnarok/lib/tree.scm 	Update: rbtree

--- ragnarok/lib/tree.scm
+++ ragnarok/lib/tree.scm
@@ -19,11 +19,18 @@
 
 (module-export-all! (current-module))
 
+;; naive default compare function, only support integers.
+;; you must set your own compare function after class made.
+(define *default-compare*
+  (lambda (x y)
+    (- x y)))
+
 (define-class <mmr-node> ()
+  (compare #:init-value *default-compare* #:init-keyword #:compare #:getter compare #:setter compare!)
   (parent #:init-value #f #:getter parent #:setter parent!)
   (left #:init-value #f #:getter left #:setter left!)
   (right #:init-value #f #:getter right #:setter right!)
-  (key #:init-value #f #:getter key #:setter key!)
+  (key #:init-value #f #:init-keyword #:key #:getter key #:setter key!)
   (value #:init-value #f #:init-keyword #:value #:getter value #:setter value!))
 
 (define-method (grand-parent (self <mmr-node>))
@@ -36,10 +43,10 @@
   (count #:init-value 0 #:getter count #:setter count!)
   (mutex #:init-thunk make-mutex #:getter mutex))
 
-(define-method (count++ (self <mmr-tree>))
+(define-method (count++！ (self <mmr-tree>))
   (count! self (1+ (count self))))
 
-(define-method (count-- (self <mmr-tree>))
+(define-method (count--！ (self <mmr-tree>))
   (and (<= (count self) 0) (error "rb-tree: count <= 0"))
   (count! self (1- (count self))))
 
@@ -55,13 +62,12 @@
 (define-method (unlock-tree! (self <mmr-tree>))
   (unlock-mutex (mutex self)))
 
-
 ;; ------- Red&Black tree
 (define RED 0)
 (define BLACK 1)
 
 (define-class <mmr-rbnode> (<mmr-node>)
-  (color #:init-value RED #:init-keyword #:color #:getter color #:setter color!))
+  (color #:init-value #f #:init-keyword #:color #:getter color #:setter color!))
 
 (define-method (black? (self <mmr-rbnode>))
   (= (color self) BLACK))
@@ -70,13 +76,26 @@
   (= (color self) RED))
 
 (define-method (root? (self <mmr-rbnode>))
-  (parent self))
+  (and (not (parent self)) 
+       (or (black? self) (error "bad root node!" self))))
 
 (define-method (leaf? (self <mmr-rbnode>))
   (and (not (right self)) 
        (not (left self))
        (= (color self) BLACK)))
 
+(define new-black-node
+  (lambda (k v)
+    (make <mmr-rbnode> #:color BLACK #:key k #:value v)))
+
+(define new-black-node
+  (lambda (k v)
+    (make <mmr-rbnode> #:color RED #:key k #:value v)))
+
+(define new-rb-root-node new-black-node)
+(define new-rb-node new-red-node)
+
+;;--- rb tree ---
 (define-class <mmr-rbtree> (<mmr-rbnode>))
 
 (define-class (initialize (self <mmr-rbtree>) . args)
@@ -86,19 +105,188 @@
 (define-method (empty? (self <mmr-rbtree>))
   (leaf? self))  ;; if root is leaf, it means an empty tree.
 
-(define-method (insert (self <mmr-rbtree>) elem)
-  (let ((node (make #:value elem)))
-    (if (empty? self)
-	(value! self elem)
-	(begin
-	  (rb-tree-insert-node self node)
-	  (color! self BLACK))
-	(count++ self)
-	node)))
+(define-method (insert (self <mmr-rbtree>) k v)
+  (insert self (make #:key k #:value v)))
 
-(define rb-tree-insert-node
-  (lambda (self node)
-    #t))
+(define-method (insert (self <mmr-rbtree>) (node <mmr-rbnode>))
+  (if (empty? self)
+      (value! self node)
+      (let ((it (inner-insert self (key node) (value node))))
+	(inner-fixup it)
+	(color! self BLACK))
+      (count++! self)
+      node))
+
+(define-method (search (self <mmr-rbtree>) key)
+  (if (empty? self) 
+      #f
+      (let ((r ((compare self) key (key self))))
+	(cond
+	 ((> r 0) (search (right self) comp key))
+	 ((< r 0) (search (left self) comp key))
+	 (else (value self))))))
+
+(define insert-left!
+  (lambda (rbt k v)
+    (left! rbt (new-rb-node k v))
+    (parent! (left rbt) rbt)))
+
+(define insert-right!
+  (lambda (rbt k v)
+    (right! rbt (new-rb-node k v))
+    (parent! (right rbt) rbt)))
+
+(define inner-insert
+  (lambda (rbt )
+    (let ((r (comp k v)))
+    (cond
+     ((< r 0) 
+      (if (leaf? (left rbt))
+	  (begin
+	    (insert-left! rbt k v)
+	    rbt)
+	  (inner-insert (left rbt) k v)))
+     ((> r 0) 
+      (if (leaf? (right rbt))
+	  (begin
+	    (insert-right! rbt k v)
+	    rbt)
+	  (inner-insert (rb-node:right en) k v)))
+     (else (rb-node:value! en v) en)))))
+
+(define inner-fixup
+  (lambda (rbt)
+    (cond 
+     ((and (red? (right rbt))
+           (red? (left rbt)))
+      (rb-tree-flip-color rbt)
+      (inner-fixup rbt))
+     ((and (red? (right rbt))
+           (black? (left rbt)))
+      (inner-fixup (rb-tree-rotate-left-1 rbt)))
+     ((and (red? (left rbt))
+           (red? (left (left rbt))))
+      (inner-fixup (rb-tree-rotate-right-1 rbt)))
+     ((root? rbt) rbt)
+     (else
+      (inner-fixup (parent rbt))))))
+
+(define color-flip!
+  (lambda (n)
+    (unless (leaf? n)
+      (let ([c (color n)])
+	(color! n (flip c)))))) 
+
+(define flip
+  (lambda (x)
+    (logxor x 1)))
+
+(define rb-tree-flip-color
+  (lambda (nn)
+    (color-flip! nn)
+    (color-flip! (left nn))
+    (color-flip! (right nn))))
+
+(define set-parent!
+  (lambda (n p)
+    (unless (leaf? n)
+            (parent! n p))))
+
+(define set-child-left!
+  (lambda (n1 n2)
+    (unless (leaf? n1)
+            (left! n1 n2))))
+
+(define set-child-right!
+  (lambda (n1 n2)
+    (unless (leaf? n1)
+            (right! n1 n2))))
+
+(define right-child?
+  (lambda (n)
+    (if (leaf? n)
+        #f
+        (let ([np (parent n)])
+          (if (leaf? np)
+              #f
+              (eq? (right np) n))))))
+
+(define set-red!
+  (lambda (n)
+    (unless (leaf? n)
+            (color! n RED))))
+
+(define set-black!
+  (lambda (n)
+    (unless (leaf? n)
+            (color! n BLACK))))
+
+;; n1->color = n2->color
+(define get-color-from!
+  (lambda (n1 n2)
+    (unless (leaf? n1)
+            (if (leaf? n2)
+                (set-black! n1)
+                (color! n1 (color n2))))))
+
+(define rb-tree-rotate-left-1
+  (lambda (rbt)
+    (let ((x (right rbt)))
+      (right! rbt (left x))  ;; rbt->right = x->left
+      (set-parent! (right rbt) rbt)  ;; rbt->right->parent = rbt
+      
+      (left! x rbt)  ;; x->left = rbt
+      
+      (if (right-child? rbt)
+	  (set-child-right! (parent rbt) x)
+	  (set-child-left! (parent rbt) x))
+      
+      (set-parent! x (parent rbt))  ;; x->parent = rbt->parent
+      (set-parent! rbt x)  ;; rbt->parent = x
+      
+      (get-color-from! x rbt)
+      (set-red! rbt)
+      ;;(format #t "rb-tree-rotate-left: en:~a~%" x)
+      x)))
+
+(define rb-tree-rotate-right-1
+  (lambda (rbt)
+    (let ((x (left rbt)))
+      (left! rbt (right x))  ;; rbt->left = x->right
+      (set-parent! (left rbt) rbt)  ;; rbt->left->parent = rbt
+
+      (right! x rbt)  ;; x->right = rbt
+
+      (if (right-child? rbt)
+          (set-child-right! (parent rbt) x)
+          (set-child-left! (parent rbt) x))
+
+      (set-parent! x (parent rbt)) ;; x->parent = rbt->parent
+      (set-parent! rbt x)  ;; rbt->parent = x
+      
+      (get-color-from! x rbt)
+      (set-black! rbt)
+      ;;(format #t "rb-tree-rotate-right: en:~a~%" x)
+      x)))
+
+(define move-red-left
+  (lambda (rbt)
+    (rb-tree-flip-color rbt)
+    (if (red? (left (right rbt)))
+	(begin
+	  (right! rbt (rb-tree-rotate-right-1 (right rbt)))
+	  (set! rbt (rb-tree-rotate-left-1 rbt))
+	  (rb-tree-flip-color rbt))
+	rbt)))
+
+(define move-red-right
+  (lambda (rbt)
+    (rb-tree-flip-color rbt)
+    (if (red? (left (left rbt)))
+	(begin
+	  (set! rbt (rb-tree-rotate-right-1 rbt))
+	  (rb-tree-flip-color rbt))
+	rbt)))
 
 ;; TODO: finish rbtree insert/search
 
