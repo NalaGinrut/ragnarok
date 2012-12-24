0305b6379ddbfa711f0b01fcde8cbece052c6198
Nala Ginrut <nalaginrut@gmail.com>
2012-12-24 17:52:20 +0800

    Delimited-continuation block almostly done!

--- ragnarok/actors.scm
+++ ragnarok/actors.scm
@@ -15,8 +15,17 @@
 
 (define-module (ragnarok actors)
   #:use-module (ragnarok utils)
+  #:use-module (ragnarok error)
   #:use-module (ice-9 control)
   #:export ())
+
+(define (table-in! tb k v)
+  (hash-set! tb k v))
+
+(define (table-out! tb k)
+  (let ((v (hash-ref tb k)))
+    (hash-set! tb k #f)
+    v))
 
 (define make-queue
   (lambda (capacity)
@@ -47,11 +56,11 @@
 (define-syntax !
   (syntax-rules (<= :)
     ((_ to <= from : msg)
-     (let ((actor (to to from msg)))
-       (! actor)))
-    ((_ to <= from : msg ? condition) ;; send and wait for 'condition'
-     (let ((actor (to to from `(wait-for ,condition))))
-       (! actor)))
+     (let ((actor (% (to to from msg)
+		     (lambda (k) k))))
+       (if (thunk? actor) ;; if #t, then it's a delimited-continuation
+	   (to to to 'would-block actor) ;; add itself to boss' bt
+	   (! actor))))
     ((_ who) ;; schedule 'who'
      (! who <= who : 'yield))))
 
@@ -61,9 +70,9 @@
 ;; 4. NOTE: we don't have explicit 'running' status, if it's working in progress,
 ;;          it's runing, but we don't have a status valued 'running', only 'ready'
 ;;          actor could be a 'running' one.
-(define* (make-actor director #:key (name (gensym "actor-")) 
+(define* (make-actor director #:key (name (gensym "actor-")) (wait-for #f)
 		     (box-room 100) (boss #f) (script #f) (status 'ready)
-		     (wq (make-queue #f)) (bq (make-queue #f)) (fq (make-queue #f)))
+		     (wq (make-queue #f)) (fq (make-queue #f)) (bt (make-hash-table)))
   (let ((mail-box (make-mail-box box-room))
 	(start-sleep 0)	(sleep-time 0))
     (lambda (self from . new-msg)
@@ -73,7 +82,15 @@
 	  (if boss
 	      (sleep 10) ;; OK, I lied, boss can sleep when there's no work, but it won't response for any sleep command.
 	      (! director <= self `(schedule ,self)))) ;; no msg, re-schedule
-	 ((eq? status 'blocked) (! from <= self `(resend ,m))) ;; resend if blocked
+	 ((eq? status 'blocked)
+	  (if wait-for
+	      (catch #t (lambda () (peek-char wait-for))
+		(lambda e
+		  (let ((E (get-errno e)))
+		    (cond
+		     ((or (= E EAGAIN) (= E EWOULDBLOCK)) 
+		      (! from <= self `(resend ,m))) ;; resend if still blocked
+		     (else (set! wait-for #f) (! director <= self 'raise-me))))))))
 	 ((eq? status 'sleep)
 	  (cond
 	   ((>= (- (current-time) start-sleep) sleep-time)
@@ -81,15 +98,17 @@
 	    (! self <= director : 'run))))
 	 ((eq? status 'free) (! from <= self 'dead)) ;; dead must handle in script
 	 (boss
-	  (case (msg-content msg) ;; boss never response to these messages
-	    ((wake-up) (queue-in! wq (from-who msg)))
-	    ((stun-me) (queue-in! sq (from-who msg)))
-	    ((block-me) (queue-in! bq (from-who msg)))
-	    ((free-me) (and fq (queue-in! fq (from-who msg))))
-	    ((schedule-me) (queue-in! wq (from-who msg)))
-	    ((sleep block) (error "boss never sleep/block!" m))
-	    ((run) (error "boss is working, go back to work!" m))
-	    ((yield) (lp (mail-box-out! mail-box))) ;; boss never yield, keep working
+	  (match (msg-content msg) ;; boss never response to these messages
+	    ('wake-up (queue-in! wq (from-who msg)))
+	    ('stun-me (queue-in! sq (from-who msg)))
+	    (`(block-me ,dc) (table-in! bt (from-who msg) dc))
+	    ('raise-me ((table-out! bt (from-who msg)))) ;; raise the dc from bt then run it
+	    ('free-me (and fq (queue-in! fq (from-who msg))))
+	    ('schedule-me (queue-in! wq (from-who msg)))
+	    ('sleep (error "boss never sleep!" m))
+	    ('block (error "boss never block!" m))
+	    ('run (error "boss is working, go back to work!" m))
+	    ('yield (lp (mail-box-out! mail-box))) ;; boss never yield, keep working
 	    ;; TODO: add others?
 	    ))	 
 	 (else
@@ -107,7 +126,7 @@
 	       (set! sleep-time time)
 	       (set! start-sleep (current-time))
 	       (! director <= self : 'stun-me))
-	      ('block (set! status 'blocked) (! director <= self : 'block-me))
+	      (`(would-block ,dc) (set! status 'blocked) (! director <= self : 'block-me dc))
 	      ('yield (set! status 'ready) (! director <= self : 'schedule-me))
 	      ('release (set! status 'free) 
 		       (for-each clear! (list mail-box wq sq bq)) 
@@ -116,7 +135,7 @@
 	       (for-each (lambda (a) 
 			   (module-set! (current-module) (car k) (cadr v)))
 			 kv-pairs))	      
-	      (`(take-over ,actor) (set! director actor) (! self))	      
+	      ('take-over (set! director sender) (! self))	      
 	      ;; TODO: other msg handler
 	      (else (and script (script self director from msg)))))))
 	;; ENHANCEME: the policy is naive: schedule me after 'one msg' is handled
@@ -128,6 +147,7 @@
   (lambda (name script)
     (let ((boss (make-actor #f ;; boss' director is himself, we'll do it later
 			    #:name name
+			    #:wait-for #f ;; never used by boss
 			    #:box-room #f ;; infinite actors it will control
 			    #:wq (make-queue #f) ;; infinite workers
 			    #:bq (make-queue #f) ;; infinite blocked actors
@@ -138,11 +158,12 @@
 			    )))
       ;; boss has to take over himself, poor man...
       ;; and this operation will return the boss as an actor
-      (! boss <= 'take-over boss))))
+      (! boss <= boss : 'take-over))))
 
 (define new-actor 
   (lambda (name script director workers)
     (let ((actor (make-actor director #:name name
+			     #:wait-for #f ;; the buffered port which is blocked, and need break
 			     #:box-room #f ;; infinite actors it will control
 			     #:wq (make-queue workers) ;; infinite workers
 			     #:bq (make-queue workers) ;; infinite blocked actors
@@ -155,8 +176,8 @@
       )))
 
 (define-syntax script-wrapper
-  (syntax-rules ()
-    ((_ pattern patterns* ...)
+  (syntax-rules (<=)
+    ((_ <= pattern patterns* ...)
      (lambda (script self director from msg)
        (cond
 	((not msg) (error "invalid message" msg))
